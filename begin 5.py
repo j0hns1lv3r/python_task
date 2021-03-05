7eb93f0857be1cb87fe76ec0c9dde0492d5f8061
j0hns1lv3r <77633044+j0hns1lv3r@users.noreply.github.com>
2021-03-05 23:15:31 +0200

    Add files via upload

---
+++ begin 5.py
@@ -0,0 +1,8 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======== RESTART: C:/Users/Николай/Downloads/ghjuhfvvbhjdfybz/begin 4.py =======
+введите ребро куба 3
+объем =  27
+площадь поверхности =  54
+>>> 
